cd823b2ddfbdd9f559a36218e2325108460a06a8
gusalecar <gc.git@outlook.com>
2022-05-24 11:12:20 -0300

    Enforce / path separator

--- classes/resolver.py
+++ classes/resolver.py
@@ -6,8 +6,14 @@ def get_local_imports(playbook):
     obj = yaml.safe_load(playbook)
     ret: list[str] = obj.get("imports", [])
 
+    if not ret:
+        return ret
+
     if not isinstance(ret, list) and not all(isinstance(i, str) for i in ret):
         raise Exception("Invalid imports")
+
+    if any(i for i in ret if "\\" in i):
+        raise Exception("Imports must use / as path separator")
 
     for ref in [i for i in ret if not i.startswith("satori://")]:
         if not os.path.isfile(ref) or not os.path.isdir(ref):
