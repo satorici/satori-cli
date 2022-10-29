ae104add36c74e246ce27db6fa10835eb6791723
fearcito <my@fake.email>
2022-10-29 12:03:08 -0300

    pip bugfixes

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='satori-ci',
-    version='1.0',
+    version='1.0.0a',
     license='MIT',
     author="Satori CI",
     author_email='info@satori-ci.com',
+    long_description="Satori is an automated testing platform that helps people reduce risks and costs"
     #packages=find_packages('classes'),
     package_dir={'main_package': 'src'},
     url='https://github.com/satorici/satori-cli',
@@ -21,5 +22,9 @@ setup(
           'requests>=2.28.1',
           'urllib3>=1.26.12'
       ],
-
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Testing',
+    ],
 )
