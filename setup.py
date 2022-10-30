81c68a18656ef0c884c157ded84759785ed736ff
fearcito <my@fake.email>
2022-10-30 11:35:59 -0300

    bugs

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
-from setuptools import setup, find_packages
+from setuptools import setup #, find_packages
 
 
+setup()
+
+"""
 setup(
     name='satori-ci',
-    version='1.0.0a',
-    license='MIT',
-    author="Satori CI",
-    author_email='info@satori-ci.com',
-    long_description="Satori is an automated testing platform that helps people reduce risks and costs"
+    #packages = ['satori-ci']
+    version = '1.0.0a',
+    license = 'MIT',
+    author = "Satori CI",
+    author_email = 'info@satori-ci.com',
+    long_description = "Satori is an automated testing platform that assists in reducing risks and costs."
     #packages=find_packages('classes'),
-    package_dir={'main_package': 'src'},
-    url='https://github.com/satorici/satori-cli',
-    keywords='Satori CI CLI',
-    install_requires=[
-          'scikit-learn',
+    package_dir = {'main_package': 'src'},
+    url = 'https://github.com/satorici/satori-cli',
+    keywords = ['Satori CI', 'testing', 'CLI'],
+    install_requires = [
           'certifi>=2022.9.24',
           'charset-normalizer>=2.1.1',
           'idna>=3.4',
@@ -22,9 +25,10 @@ setup(
           'requests>=2.28.1',
           'urllib3>=1.26.12'
       ],
-    classifiers=[
+    classifiers = [
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3.9',
         'Topic :: Software Development :: Testing',
     ],
 )
+"""
