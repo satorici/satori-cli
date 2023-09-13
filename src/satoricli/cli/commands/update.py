cf043ea6465d1190b0a66cfe201446334775df07
gusalecar <gc.git@outlook.com>
2023-09-13 13:16:41 -0300

    Remove update confirmation

--- src/satoricli/cli/commands/update.py
+++ src/satoricli/cli/commands/update.py
@@ -2,8 +2,6 @@ import subprocess
 import sys
 from argparse import ArgumentParser
 
-from rich.prompt import Confirm
-
 from satoricli.cli.utils import console
 
 from .base import BaseCommand
@@ -13,18 +11,10 @@ class UpdateCommand(BaseCommand):
     name = "update"
 
     def register_args(self, parser: ArgumentParser):
-        parser.add_argument(
-            "-y",
-            action="store_true",
-            dest="confirmed",
-            help="Update without confirmation",
-        )
+        pass
 
     def __call__(self, confirmed: bool, **kwargs):
         console.print(f"Going to run: {sys.executable} -m pip install -U satori-ci")
-
-        if not confirmed and not Confirm.ask("Do you want to continue", default="y"):
-            return
 
         proc = subprocess.run(
             [sys.executable, "-m", "pip", "install", "-U", "satori-ci"],
