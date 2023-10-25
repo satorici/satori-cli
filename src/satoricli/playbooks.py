ce5aee5f94823ac68423167e45514324bbf5a4f4
gusalecar <gc.git@outlook.com>
2023-10-24 22:29:54 -0300

    Simplify public playbooks related code
    * Ignore .github dir
    * Use pathlib.Path for everything
    * Use snake case

--- src/satoricli/playbooks.py
+++ src/satoricli/playbooks.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 from typing import Optional
 
@@ -7,8 +6,10 @@ import yaml
 from .cli.utils import autosyntax, autotable, console
 from .validations import get_parameters
 
+PLAYBOOKS_DIR = Path.home() / ".satori/playbooks"
 
-def clone(directoryName: Path):
+
+def sync():
     """Clone or pull the playbooks repo"""
 
     try:
@@ -18,87 +19,62 @@ def clone(directoryName: Path):
             "The git package could not be imported.",
             "Please make sure that git is installed in your system.",
         )
-        return 1
+        return False
 
-    if directoryName.is_dir():
-        repo = git.Repo(directoryName)
+    if PLAYBOOKS_DIR.is_dir():
+        repo = git.Repo(PLAYBOOKS_DIR)
         repo.branches["main"].checkout(force=True)
         repo.remote().pull()
         print("Satori Playbooks repo updated to latest vesrsion")
     else:
-        git.Repo.clone_from("https://github.com/satorici/playbooks.git", directoryName)
+        git.Repo.clone_from("https://github.com/satorici/playbooks.git", PLAYBOOKS_DIR)
         print("Satori Playbooks repo clone started")
-    return 0
+    return True
 
 
-def file_finder(directoryName) -> list[dict]:
-    """File finder"""
+def file_finder() -> list[dict]:
     playbooks = []
-    list_dirs = os.walk(directoryName)
-    for root, dirs, files in list_dirs:
-        dirs = ""
-        for d in dirs:
-            dirs += os.path.join(root, d)
-        dirs_printed = False
-        for f in files:
-            if f.endswith(".yml") and f != ".satori.yml":
-                if dirs_printed is False:
-                    dirs_printed = True
 
-                filenames = os.path.join(root, f)
+    for playbook in PLAYBOOKS_DIR.rglob("*.yml"):
+        if ".github" in playbook.parts:
+            continue
 
-                parameters = []
-                with open(filenames) as file:
-                    try:
-                        parameters = get_parameters(yaml.safe_load(file))
-                    except Exception:
-                        pass
+        try:
+            parameters = get_parameters(yaml.safe_load(playbook.read_text()))
+        except Exception:
+            parameters = ()
 
-                names = get_playbook_name(filenames)
-                playbooks.append(
-                    {
-                        "filename": filenames.replace(str(directoryName), "satori:/"),
-                        "name": names,
-                        "parameters": ", ".join(parameters),
-                    }
-                )
+        playbooks.append(
+            {
+                "filename": str(playbook).replace(str(PLAYBOOKS_DIR), "satori:/"),
+                "name": get_playbook_name(playbook),
+                "parameters": ", ".join(parameters),
+            }
+        )
+
     return playbooks
 
 
-def get_playbook_name(filename):
-    """Function to load files desc in list"""
-    playbook = ""
-    with open(filename, "r") as file:
-        try:
-            playbook = yaml.safe_load(file)
-        except yaml.YAMLError:
-            pass
-        if "settings" in playbook and "name" in playbook["settings"]:
-            return playbook["settings"]["name"]
-        else:
-            return "None"
+def get_playbook_name(filename: Path):
+    try:
+        config = yaml.safe_load(filename.read_text())
+        return config["settings"]["name"]
+    except Exception:
+        pass
 
 
 def display_public_playbooks(playbook_id: Optional[str] = None) -> None:
-    directoryName = Path.home() / ".satori/playbooks"
+    if not sync():
+        return
 
-    if clone(directoryName) == 0:
-        playbooks = file_finder(directoryName)
+    if not playbook_id:  # satori playbook --public
+        playbooks = file_finder()
         playbooks.sort(key=lambda x: x["filename"])
+        autotable(playbooks)
+    else:  # satori playbook satori://x
+        path = PLAYBOOKS_DIR / playbook_id.removeprefix("satori://")
 
-        if not playbook_id:  # satori playbook --public
-            # Print table with playbooks data by default
-            autotable(playbooks)
-        else:  # satori playbook satori://x
-            # print the content of a playbook
-            playbook_path = None
-            for playbook in playbooks:
-                if playbook["filename"] == playbook_id:
-                    playbook_path = Path(
-                        str(directoryName)
-                        + playbook["filename"].replace("satori://", "/")
-                    )
-                    autosyntax(playbook_path.read_text(), lexer="YAML")
-                    break
-            if playbook_path is None:
-                console.print("[red]Playbook not found")
+        if path.is_file():
+            autosyntax(path.read_text(), lexer="YAML")
+        else:
+            console.print("[red]Playbook not found")
