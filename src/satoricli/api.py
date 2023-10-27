571662dda1b76f8e7a2f15c53d50082126fe45d8
Mariano Carrazana <marianocarrazana@gmail.com>
2023-10-27 14:24:58 -0300

    Show errors details

--- src/satoricli/api.py
+++ src/satoricli/api.py
@@ -1,14 +1,33 @@
+import sys
 from typing import Optional
 
 import httpx
+from rich.console import Console
 
 HOST = "https://api.satori-ci.com"
 WS_HOST = "wss://api.satori-ci.com"
 
+
+def error_handler(r: httpx.Response):
+    if r.is_error:
+        console = Console()
+        try:
+            r.read()
+            error_data: dict = r.json()
+        except Exception:
+            console.print("[b]An unknown error ocurred")
+        else:
+            console.print(
+                "[b]An error ocurred:[/] " + error_data.get("detail", "Unknown error")
+            )
+        console.print("[b]Status code:[/] " + str(r.status_code))
+        sys.exit(1)
+
+
 client = httpx.Client(
     base_url=HOST,
     follow_redirects=True,
-    event_hooks={"response": [lambda r: r.raise_for_status()]},
+    event_hooks={"response": [error_handler]},
     timeout=60,
 )
 
