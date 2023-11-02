8dd22d9756c7f511273c2e9ccecd75a1d126d47c
Mariano Carrazana <marianocarrazana@gmail.com>
2023-11-02 15:38:36 -0300

    Remove strip() from stdout/stderr

--- src/satoricli/cli/utils.py
+++ src/satoricli/cli/utils.py
@@ -404,14 +404,10 @@ def format_outputs(outputs):
         console.print("[blue]Return code:[/blue]", output["output"]["return_code"])
         console.print("[blue]Stdout:[/blue]")
         if output["output"]["stdout"]:
-            console.out(
-                b64decode(output["output"]["stdout"]).decode(errors="ignore").strip()
-            )
+            console.out(b64decode(output["output"]["stdout"]).decode(errors="ignore"))
         console.print("[blue]Stderr:[/blue]")
         if output["output"]["stderr"]:
-            console.out(
-                b64decode(output["output"]["stderr"]).decode(errors="ignore").strip()
-            )
+            console.out(b64decode(output["output"]["stderr"]).decode(errors="ignore"))
 
 
 @dataclass
