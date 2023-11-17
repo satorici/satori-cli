25763e2fb178647d90351ae68d2b1c16b08b10d3
Mariano Carrazana <marianocarrazana@gmail.com>
2023-11-17 12:45:04 -0300

    Show "Execution time" in reports

--- src/satoricli/cli/commands/report.py
+++ src/satoricli/cli/commands/report.py
@@ -83,7 +83,7 @@ class ReportCommand(BaseCommand):
                 [
                     ["Execution type", report["execution"], "bright_magenta"],
                     ["Public", report["public"]],
-                    ["Time required", report["time required"]],
+                    ["Execution time", report.get("execution_time")],
                     ["User", report["user"]],
                     ["Date", report["date"].replace("T", " ")],
                     ["Status", report["status"]],
@@ -148,7 +148,7 @@ class ReportCommand(BaseCommand):
                 ["Execution type", report["execution"], "bright_magenta"],
                 ["Public", report["public"]],
                 ["Source", report["source"]],
-                ["Time required", report.get("execution_time")],
+                ["Execution time", report.get("execution_time")],
                 ["Monitor", report["monitor_id"]],
                 ["Date", report["created"].replace("T", " ")],
                 ["Fails", report["fails"]],
