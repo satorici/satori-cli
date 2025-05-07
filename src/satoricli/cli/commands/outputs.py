33a86047efa4d0adf4c86a5aeec47baa4d2b4ac0
marian <marianocarrazana@gmail.com>
2025-05-07 20:46:36 -0300

    Add loop to download all outputs Extract {report_id}-files.tar.gz if exist

--- src/satoricli/cli/commands/outputs.py
+++ src/satoricli/cli/commands/outputs.py
@@ -1,14 +1,17 @@
 import io
+import itertools
 import os
 import tarfile
+import time
 from argparse import ArgumentParser
 from datetime import date
 from typing import Literal, Optional, get_args
 
 import httpx
+from rich.progress import Progress, SpinnerColumn, TextColumn
 
-from satoricli.api import client
-from satoricli.cli.utils import console, format_outputs
+from satoricli.api import client, disable_error_raise
+from satoricli.cli.utils import console, error_console, format_outputs
 
 from ..arguments import date_args
 from .base import BaseCommand
@@ -106,21 +109,21 @@ class OutputsCommand(BaseCommand):
                             format_outputs(stream.iter_lines())
                             console.print()
         elif action == "export":
-            res = client.get(
-                "/outputs/export",
-                params={
-                    "playbook_type": capitalize(playbook_type),
-                    "report_visibility": capitalize(report_visibility),
-                    "result": capitalize(result),
-                    # "query": query,
-                },
-            )
+            params = {
+                "playbook_type": capitalize(playbook_type),
+                "report_visibility": capitalize(report_visibility),
+                "result": capitalize(result),
+                # "query": query,
+                "limit": 10,
+                "page": 1,
+            }
 
             # Save response
             extract_dir = name if name else "export"
 
             if raw:
                 # Save to file
+                res = client.get("/outputs/export", params=params)
                 with open(f"{extract_dir}.tar.gz", "wb") as f:
                     f.write(res.content)
                     console.print(
@@ -128,7 +131,7 @@ class OutputsCommand(BaseCommand):
                     )
                 return
 
-            # Untar file
+            # Create directory if dont exit
             if not os.path.exists(extract_dir):
                 os.makedirs(extract_dir)
                 console.print(f"Created directory: {extract_dir}")
@@ -138,15 +141,45 @@ class OutputsCommand(BaseCommand):
                     "\nRemove it manually to avoid conflicts"
                 )
 
-            # Use io.BytesIO to treat the bytes as a file-like object
-            with io.BytesIO(res.content) as tar_buffer:
-                with tarfile.open(fileobj=tar_buffer, mode="r:*") as tar:
-                    tar.extractall(path=extract_dir, filter="data")
-                console.print(
-                    f"Successfully dowloaded outputs to folder [b]{extract_dir}"
-                )
+            with Progress(
+                SpinnerColumn("dots12"),
+                TextColumn(
+                    "[progress.description]Outputs downloaded: {task.description}"
+                ),
+                console=error_console,
+            ) as progress:
+                task = progress.add_task(" - ")
 
-            # List all extracted files
-            for root, _, files in os.walk(extract_dir):
-                for file in files:
-                    console.print(os.path.join(root, file))
+                for i in itertools.count(start=1):
+                    progress.update(task, description=f"{(i - 1) * 10}")
+                    params["page"] = i
+                    with disable_error_raise() as c:
+                        res = c.get("/outputs/export", params=params)
+
+                    if res.is_error:
+                        progress.stop()
+                        # List all extracted files
+                        for root, _, files in os.walk(extract_dir):
+                            for file in files:
+                                console.print(os.path.join(root, file))
+                        return 0
+
+                    # Use io.BytesIO to treat the bytes as a file-like object
+                    with (
+                        io.BytesIO(res.content) as tar_buffer,
+                        tarfile.open(fileobj=tar_buffer, mode="r:*") as tar,
+                    ):
+                        tar.extractall(path=extract_dir, filter="data")
+
+                    # Extract files
+                    for item in os.listdir(extract_dir):
+                        item_path = os.path.join(extract_dir, item)
+                    if os.path.isfile(item_path) and item.endswith(".tar.gz"):
+                        base_name = item[:-7]  # Remove the ".tar.gz" extension
+                        output_folder = os.path.join(extract_dir, base_name)
+                        # Create the output folder if it doesn't exist
+                        os.makedirs(output_folder, exist_ok=True)
+                        with tarfile.open(item_path, "r:gz") as tar:
+                            tar.extractall(path=output_folder, filter="data")
+                        os.remove(item_path)
+                    time.sleep(1)
