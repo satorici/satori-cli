4b9fdf75ccf6953b7b0c1ac2cc62e82bff8a5f2a
marian <marianocarrazana@gmail.com>
2025-06-12 15:02:10 -0300

    Install last version from pypi

--- src/satoricli/cli/commands/update.py
+++ src/satoricli/cli/commands/update.py
@@ -3,6 +3,8 @@ import subprocess
 import sys
 from argparse import ArgumentParser
 
+import httpx
+
 from satoricli.cli.utils import console
 
 from .base import BaseCommand
@@ -17,7 +19,11 @@ class UpdateCommand(BaseCommand):
     def __call__(self, **kwargs):
         console.print(f"Going to run: {sys.executable} -m pip install -U satori-ci")
 
-        args = [sys.executable, "-m", "pip", "install", "-U", "satori-ci"]
+        # Get last version from pypi and avoid to install it from cache
+        response = httpx.get("https://pypi.org/pypi/satori-ci/json")
+        latest = response.json()["info"]["version"]
+
+        args = [sys.executable, "-m", "pip", "install", "-U", f"satori-ci=={latest}"]
 
         if platform.system() == "Windows":
             subprocess.Popen(args)
