9feeed70e3d6c2be624ec8fd76430808cf4301ac
marian <marianocarrazana@gmail.com>
2025-10-01 18:27:21 -0300

    Add whoami command

---
+++ src/satoricli/cli/commands/whoami.py
@@ -0,0 +1,35 @@
+from argparse import ArgumentParser
+from typing import Literal, get_args
+
+from satoricli.api import client
+from satoricli.cli.utils import (
+    console,
+)
+
+from .base import BaseCommand
+
+ACTIONS = Literal["show"]
+
+
+class WhoamiCommand(BaseCommand):
+    name = "whoami"
+
+    def register_args(self, parser: ArgumentParser):
+        parser.add_argument(
+            "action", nargs="?", choices=get_args(ACTIONS), default="show"
+        )
+
+    def __call__(
+        self,
+        action: ACTIONS,
+        **kwargs,
+    ):
+        if action == "show":
+            info = client.get("/users/me").json()
+            console.print("Name:", info["name"])
+            console.print("Display name:", info["display_name"])
+            console.print("Email:", info["email"])
+            console.print("Role:", info["role"].capitalize())
+            console.print("Team:", info["team_name"])
+
+        return
